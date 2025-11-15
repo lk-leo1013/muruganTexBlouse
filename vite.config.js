389e57acde6fbc6de28428759b7213375a48250b
lk-leo1013 <lkbsac1013@gmail.com>
2025-11-15 22:25:38 +0530

    modified vite config

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [react()],
-    base: command === 'build' ? '/muruganTexBlouse/' : '/', // ⚠ repo name here
-  }
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
 })
